2425ed3938500c731f074eba97b9d2a49495626b
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2022-03-12 18:06:55 +0800

    Update ustclogin2.py

--- ustclogin2.py
+++ ustclogin2.py
@@ -19,25 +19,24 @@ class Login:
             'model': 'uplogin.jsp',
             'service': unquote(self.service),
             'warn': '',
-            'showCode': '0',
+            'showCode': '',
             'username': self.stuid,
             'password': str(self.password),
             'button': '',
             'CAS_LT':CAS_LT,
             'LT':''
         }
-        post=self.session.post('https://passport.ustc.edu.cn/login', data=data,headers=headers)
-        return post
+        self.result=self.session.post('https://passport.ustc.edu.cn/login', data=data,headers=headers)
         
     def login(self):
         self.session=requests.Session()
         loginsuccess = False
         retrycount = 5
         while (not loginsuccess) and retrycount:
-            result=self.passport()
+            self.passport()
             self.cookies = self.session.cookies
             retrycount = retrycount - 1
-            if result.url=='https://passport.ustc.edu.cn/login':
+            if self.result.url=='https://passport.ustc.edu.cn/login':
                 print("Login Failed! Retry...")
             else:
                 print("Login Successful!")
