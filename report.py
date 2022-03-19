8ccb19fd7da08dcf58f1d810f539f450da9a0686
Kobe972 <71507738+Kobe972@users.noreply.github.com>
2022-03-19 10:13:07 +0800

    Add files via upload

--- report.py
+++ report.py
@@ -26,7 +26,7 @@ class Report(object):
             soup = BeautifulSoup(data, 'html.parser')
             token = soup.find("input", {"name": "_token"})['value']
 
-            with open(self.data_path, "r+") as f:
+            with open(self.data_path, "r+", encoding='utf-8') as f:
                 data = f.read()
                 data = json.loads(data)
                 data["_token"]=token
